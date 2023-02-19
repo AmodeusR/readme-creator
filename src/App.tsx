d2fa58ef134dfd85bacd8b2488b9d4169b52f14d
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-02-18 21:13:47 -0300

    Update App with routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { RouterProvider } from "react-router-dom";
+import { routes } from "./routes";
 
 function App() {
 
   return (
     <div className="App">
-      <h1>こんにちは、世界！</h1>
+      <RouterProvider router={routes} />
     </div>
   );
 }
