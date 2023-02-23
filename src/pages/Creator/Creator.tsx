213d71e4393250972cc62177392b2304b518e7d6
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-02-22 21:24:13 -0300

    Create Creator page

---
+++ src/pages/Creator/Creator.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import "./creator.scss";
+
+const availableLanguages = [
+  {
+    language: "Português",
+    langcode: "pt-br"
+  },
+  {
+    language: "English",
+    langcode: "en"
+  },
+  {
+    language: "日本語",
+    langcode: "ja"
+  },
+  {
+    language: "Deutsch",
+    langcode: "de"
+  },
+  {
+    language: "Español",
+    langcode: "es"
+  },
+];
+
+const Creator = () => {
+  const [selectedLanguage, setSelectedLanguage] = useState("pt-br");
+
+  const handleSelection = (langcode: string) => {
+    setSelectedLanguage(langcode);
+  };
+
+  return (
+    <div className="creator">
+      <h1 className="creator__title">Criador</h1>
+      <ul className="creator__languages">
+        {availableLanguages.map(language => (
+          <li key={language.langcode} className="creator__languages-option" ><button onClick={() => handleSelection(language.langcode)} type="button" className={`creator__languages-option-button ${selectedLanguage === language.langcode && "selected"} ${language.langcode === "ja" && "ja-borderfix"}`}>{language.language}</button></li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Creator;
