5f5ba50b5a96817d13b5d2384f11089665ed5394
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-02-24 20:55:17 -0300

    Update Creator with components

--- src/pages/Creator/Creator.tsx
+++ src/pages/Creator/Creator.tsx
@@ -1,51 +1,27 @@
 import { useState } from "react";
+import { CreatorForm, CreatorLanguages } from "../../components";
+import { LanguageCode } from "../../components/CreatorLanguages/CreatorLanguages";
 import "./creator.scss";
 
-type LanguageCode = "pt-br" | "en" | "ja" | "de" | "es";
-
-type AvailableLanguage = Readonly<{
-  language: string,
-  langcode: LanguageCode
-}>;
-
-const availableLanguages: AvailableLanguage[] = [
-  {
-    language: "Português",
-    langcode: "pt-br"
-  },
-  {
-    language: "English",
-    langcode: "en"
-  },
-  {
-    language: "日本語",
-    langcode: "ja"
-  },
-  {
-    language: "Deutsch",
-    langcode: "de"
-  },
-  {
-    language: "Español",
-    langcode: "es"
-  },
-];
 
 const Creator = () => {
-  const [selectedLanguage, setSelectedLanguage] = useState("pt-br");
+  const [selectedLanguage, setSelectedLanguage] = useState<LanguageCode>("pt-br");
 
   const handleSelection = (langcode: LanguageCode) => {
     setSelectedLanguage(langcode);
   };
 
   return (
-    <div className="creator">
+    <div className="creator__wrapper">
       <h1 className="creator__title">Criador</h1>
-      <ul className="creator__languages">
-        {availableLanguages.map(language => (
-          <li key={language.langcode} className="creator__languages-option" ><button onClick={() => handleSelection(language.langcode)} type="button" className={`creator__languages-option-button ${selectedLanguage === language.langcode && "selected"} ${language.langcode === "ja" && "ja-borderfix"}`}>{language.language}</button></li>
-        ))}
-      </ul>
+      <CreatorLanguages selectedLanguage={selectedLanguage} handleSelection={handleSelection} />
+      <main className="creator">
+        <CreatorForm selectedLanguage={selectedLanguage} />
+        <hr className="creator__separator" />
+        <div className="creator__preview">
+          <p>Preview</p>
+        </div>
+      </main>
     </div>
   );
 };
