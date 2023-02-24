4cb1ed358c4ebb05cdb0910995c2ee7dcf5b8ac8
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-02-24 20:54:43 -0300

    Create index for components

---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as CreatorLanguages } from "./CreatorLanguages/CreatorLanguages";
+export { default as CreatorForm } from "./CreatorForm/CreatorForm";
+export { default as MantineInput } from "./MantineInput/MantineInput";
