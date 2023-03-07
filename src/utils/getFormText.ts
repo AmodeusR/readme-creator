54aa0a46c1e10073ca37ed774bb5dd8e55a9733d
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-03-06 23:44:59 -0300

    Create function ExtendedFieldTextGetter...
    Create curry function to auto select language code

--- src/utils/getFormText.ts
+++ src/utils/getFormText.ts
@@ -1,11 +1,20 @@
-import { LanguageCode } from "../components/CreatorLanguages/CreatorLanguages";
-import { FieldContent, formLanguageTexts, LanguageOption } from "../lang/formLanguageTexts";
+import { ExtendedLanguageFields } from "./../lang/form-fields-text";
+import { LanguageCode } from "../lang/form-fields-text";
+import {
+  FieldContent,
+  formLanguageTexts,
+  LanguageFields,
+} from "../lang/form-fields-text";
 
-
-type FieldOptions = keyof LanguageOption;
+type FieldOptions = keyof LanguageFields;
+type ExtendedFieldOptions = keyof ExtendedLanguageFields;
 type ContentOptions = keyof FieldContent;
 
-export function getFormText(langcode: LanguageCode, field: FieldOptions, valueToGet: ContentOptions ) {
+export function getFieldText(
+  langcode: LanguageCode,
+  field: FieldOptions,
+  valueToGet: ContentOptions
+) {
   try {
     const valueToReturn = formLanguageTexts[langcode][field][valueToGet];
     return valueToReturn;
@@ -15,4 +24,39 @@ export function getFormText(langcode: LanguageCode, field: FieldOptions, valueTo
     }
     return formLanguageTexts["en"][field][valueToGet];
   }
+}
+
+export function curryTextGetter(
+  langcode: LanguageCode,
+  getterFunction: "standard" | "extended"
+): (field: FieldOptions, valueToGet: ContentOptions) => string | undefined {
+  if (getterFunction === "standard") {
+    return (field: FieldOptions, valueToGet: ContentOptions) => {
+      try {
+        const valueToReturn = formLanguageTexts[langcode][field][valueToGet];
+        return valueToReturn;
+      } catch (error) {
+        if (error instanceof Error) {
+          console.error(error.message);
+        }
+        return formLanguageTexts["en"][field][valueToGet];
+      }
+    };
+  } else {
+    return () => {
+      console.log("oi");
+    };
+  }
+}
+
+export function ExtendedFieldTextGetter(
+  langcode: LanguageCode,
+  field: ExtendedFieldOptions,
+  valueToGet: keyof ExtendedLanguageFields[typeof field],
+  subtitleValue?: ContentOptions
+) {
+  console.log(langcode);
+  console.log(field);
+  console.log(valueToGet);
+  console.log(subtitleValue);
 }
