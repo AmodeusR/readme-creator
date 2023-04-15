7c00c426d9e6b9d1e806b34f8d14669297ec1573
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-04-15 20:58:00 -0300

    Create Screen-Captures sub-section

--- src/components/CreatorForm/Overview.tsx
+++ src/components/CreatorForm/Overview.tsx
@@ -1,8 +1,7 @@
 import MantineInput from "../MantineInput/MantineInput";
 import ImageDropzone from "../ImageDropzone/ImageDropzone";
-import { LanguageCode } from "../../lang/form-fields-text";
-import { FieldTextGetter } from "../../utils/getFormText";
 import { SectionProps } from "./CreatorForm";
+import TagInput from "../TagInput/TagInput";
 
 const Overview = ({ getFieldText, selectedLanguage }: SectionProps) => {
   const screenCaptureStyle = {
@@ -12,10 +11,22 @@ const Overview = ({ getFieldText, selectedLanguage }: SectionProps) => {
   };
 
   return (
-    <div className="form__section">
+    <div className="form__section form__inputs">
       <h2 className="form__section-title">Sessão Geral</h2>
-      <MantineInput label={getFieldText("challenges", "value")} placeholder={getFieldText("challenges", "placeholder")} />
-      <p style={screenCaptureStyle}>Capturas de Tela</p>
+      <MantineInput
+        label={getFieldText("challenges", "value")}
+        placeholder={getFieldText("challenges", "placeholder")}
+        description={getFieldText("challenges", "description")}
+      />
+      <div>
+        <p style={screenCaptureStyle}>Capturas de Tela</p>
+        <div className="form__screen-captures">
+          <ImageDropzone selectedLanguage={selectedLanguage} />
+          <ImageDropzone selectedLanguage={selectedLanguage} />
+          <ImageDropzone selectedLanguage={selectedLanguage} />
+        </div>
+      </div>
+      <TagInput />
     </div>
   );
 };
