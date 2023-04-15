5313024d2867bcfc04fbb02c7d8a5caba766c44b
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-04-14 23:38:46 -0300

    Pre-Create Author Section

---
+++ src/components/CreatorForm/Author.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Author = () => {
+  return (
+    <div className="form__section">
+      <h2 className="form__section-title">Sessão Geral</h2>
+    </div>
+  );
+};
+
+export default Author;
