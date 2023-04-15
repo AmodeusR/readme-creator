656024acd166446f32ce56c728b77a07ed78d62d
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-04-15 20:58:43 -0300

    Add all getFieldlText options

--- src/components/CreatorForm/Header.tsx
+++ src/components/CreatorForm/Header.tsx
@@ -2,23 +2,22 @@ import MantineInput from "../MantineInput/MantineInput";
 import ImageDropzone from "../ImageDropzone/ImageDropzone";
 import { SectionProps } from "./CreatorForm";
 
-
 const Header = ({ getFieldText, selectedLanguage }: SectionProps) => {
   return (
     <div className="form__section">
       <h2 className="form__section-title">Cabeçalho</h2>
-      <div className="form__inputs">
-        <MantineInput
-          label={getFieldText("title", "value")}
-          placeholder={getFieldText("title", "placeholder")}
-        />
-        <MantineInput
-          label="Descrição"
-          placeholder={getFieldText("description", "placeholder")}
-          description={getFieldText("description", "description")}
-        />
-        <ImageDropzone selectedLanguage={selectedLanguage} />
-      </div>
+
+      <MantineInput
+        label={getFieldText("title", "value")}
+        placeholder={getFieldText("title", "placeholder")}
+        description={getFieldText("title", "description")}
+      />
+      <MantineInput
+        label="Descrição"
+        placeholder={getFieldText("description", "placeholder")}
+        description={getFieldText("description", "description")}
+      />
+      <ImageDropzone selectedLanguage={selectedLanguage} />
     </div>
   );
 };
