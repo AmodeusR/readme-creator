451baf9d55a61f7c46ad576f7f39cc65651acba8
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-04-14 23:40:24 -0300

    Update components index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as CreatorLanguages } from "./CreatorLanguages/CreatorLanguages";
 export { default as CreatorForm } from "./CreatorForm/CreatorForm";
 export { default as MantineInput } from "./MantineInput/MantineInput";
+export { default as ImageDropzone } from "./ImageDropzone/ImageDropzone";
