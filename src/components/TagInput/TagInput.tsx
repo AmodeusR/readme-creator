30c4d68d0bc93dea529db0839ee365b31d5638be
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-05-04 20:51:16 -0300

    TagInput styles & input options

--- src/components/TagInput/TagInput.tsx
+++ src/components/TagInput/TagInput.tsx
@@ -1,15 +1,75 @@
 import { useState } from "react";
 import { MultiSelect } from "@mantine/core";
+import { Tag } from "../../assets";
+import { createStyles } from "@mantine/core";
 
-const frameworks = ["React", "Vue", "Angular"];
+// Input styles
+const useStyles = createStyles(() => ({
+  icon: {
+    marginLeft: "2px",
+  },
+  value: {
+    color: "white",
+    backgroundColor: "hsl(346, 99%, 65%)",
+  },
+  defaultValueRemove: {
+    color: "white"
+  }
+}));
 
+// Input options
+const basic = ["HTML", "CSS", "Javascript", "Sass"];
+const jsFrameworks = [
+  "React",
+  "Vue",
+  "Angular",
+  "Preact",
+  "Svelte",
+  "Remix",
+  "NextJS",
+  "Gatsby",
+  "SolidJS",
+  "NuxtJS",
+];
+const cssFrameworks = ["Bootstrap", "TailwindCSS", "Semantic UI", "Bulma"];
+const componentLibraries = [
+  "Mantine",
+  "Radix UI",
+  "Material UI",
+  "Daisy UI",
+  "Chakra UI",
+];
+const backend = ["NodeJS", "Deno", "Firebase", "MySQL", "PostGRE", "SQLite"];
+const desktopAppCreators = ["Electron", "Tauri", "Flutter"];
+const others = ["Typescript", "React Native", "Ionic"];
+
+const allOptions = [
+  ...basic,
+  ...jsFrameworks,
+  ...cssFrameworks,
+  ...componentLibraries,
+  ...backend,
+  ...desktopAppCreators,
+  ...others,
+];
+
+// Input component
 const TagInput = () => {
-  const [data, setData] = useState<string[]>([...frameworks]);
-
+  const { classes } = useStyles();
+  const [chosenOptions, setChosenOptions] = useState<string[]>([]);  
 
   return (
     <div>
-      <MultiSelect data={data} label="Título" size="md"  />
+      <MultiSelect
+        data={allOptions}
+        label="Título"
+        size="md"
+        searchable
+        icon={<Tag />}
+        classNames={{ icon: classes.icon, value: classes.value, defaultValueRemove: classes.defaultValueRemove }}
+        value={chosenOptions}
+        onChange={setChosenOptions}
+      />
     </div>
   );
 };
