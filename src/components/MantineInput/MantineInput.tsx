89ccc0faa5d92975c0706164d3916bdb02ae522d
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-06-25 19:00:28 -0300

    Change label weight & realocate input styles

--- src/components/MantineInput/MantineInput.tsx
+++ src/components/MantineInput/MantineInput.tsx
@@ -1,25 +1,12 @@
-import { createStyles, TextInput, TextInputProps } from "@mantine/core";
-
-const useStyles = createStyles(() => ({
-  root: {
-    fontFamily: "inherit",
-  },
-  input: {
-    "&:focus": {
-      borderColor: "#0194FE",
-    },
-  },
-  description: {
-    margin: "-.3rem 0"
-  }
-}));
+import { TextInput, TextInputProps } from "@mantine/core";
+import { useStyles } from "@/utils/mantineStyles";
 
 const MantineInput = ({...props}: TextInputProps) => {
   const { classes } = useStyles();
 
   return (
     <TextInput
-      classNames={{ input: classes.input, description: classes.description, root: classes.root}}
+      classNames={{ input: classes.input, description: classes.description, root: classes.root, label: classes.label }}
       label="Título"
       size="md"
       {...props}
