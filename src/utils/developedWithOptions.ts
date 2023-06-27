42c4bf94de73b310790ad3c53254fe0ded297a09
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-06-26 21:59:39 -0300

    Create developedWithOptions.ts

---
+++ src/utils/developedWithOptions.ts
@@ -0,0 +1,34 @@
+const basic = ["HTML", "CSS", "Javascript", "Sass"];
+const jsFrameworks = [
+  "React",
+  "Vue",
+  "Angular",
+  "Preact",
+  "Svelte",
+  "Remix",
+  "NextJS",
+  "Gatsby",
+  "SolidJS",
+  "NuxtJS",
+];
+const cssFrameworks = ["Bootstrap", "TailwindCSS", "Semantic UI", "Bulma"];
+const componentLibraries = [
+  "Mantine",
+  "Radix UI",
+  "Material UI",
+  "Daisy UI",
+  "Chakra UI",
+];
+const backend = ["NodeJS", "Deno", "Firebase", "MySQL", "PostGRE", "SQLite"];
+const desktopAppCreators = ["Electron", "Tauri", "Flutter"];
+const others = ["Typescript", "React Native", "Ionic"];
+
+export const developedWithOptions = [
+  ...basic,
+  ...jsFrameworks,
+  ...cssFrameworks,
+  ...componentLibraries,
+  ...backend,
+  ...desktopAppCreators,
+  ...others,
+];
