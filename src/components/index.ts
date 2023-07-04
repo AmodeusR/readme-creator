0faba3a58b5faba551f390a511d504c4ce213274
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-07-03 22:06:03 -0300

    Add ItemRegister import to components index

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as CreatorForm } from "./CreatorForm/CreatorForm";
 export { default as MantineInput } from "./MantineInput/MantineInput";
 export { default as ImageDropzone } from "./ImageDropzone/ImageDropzone";
 export { default as TagInput } from "./TagInput/TagInput";
+export { default as ItemRegister } from "./ItemRegister/ItemRegister";
