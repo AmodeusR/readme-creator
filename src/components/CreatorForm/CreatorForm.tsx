c876c63d3fbb428e56dcf335da32d5ac3c6798b1
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-07-28 17:08:12 -0300

    Update CreatorForm with getExtendedFieldText

--- src/components/CreatorForm/CreatorForm.tsx
+++ src/components/CreatorForm/CreatorForm.tsx
@@ -1,4 +1,8 @@
-import { FieldTextGetter, curryTextGetter } from "../../utils/getFormText";
+import {
+  ExtendedFieldTextGetter,
+  FieldTextGetter,
+  curryTextGetter,
+} from "../../utils/getFormText";
 import { LanguageCode } from "../../lang/form-fields-text";
 import "./form.scss";
 import Header from "./Header";
@@ -7,23 +11,30 @@ import Process from "./Process";
 
 export type SectionProps = {
   getFieldText: FieldTextGetter;
-  selectedLanguage: LanguageCode
-}
-
+  getExtendedFieldText?: ExtendedFieldTextGetter;
+  selectedLanguage: LanguageCode;
+};
 
 type CreatorFormProps = {
   selectedLanguage: LanguageCode;
-}
+};
 
 const CreatorForm = ({ selectedLanguage }: CreatorFormProps) => {
   const getFieldText = curryTextGetter(selectedLanguage, "standard");
-  // const getExtendedFieldText = curryTextGetter(selectedLanguage, "extended");
+  const getExtendedFieldText = curryTextGetter(selectedLanguage, "extended");
 
   return (
     <form className="creator__form">
       <Header getFieldText={getFieldText} selectedLanguage={selectedLanguage} />
-      <Overview getFieldText={getFieldText} selectedLanguage={selectedLanguage} />
-      <Process getFieldText={getFieldText} selectedLanguage={selectedLanguage} />
+      <Overview
+        getFieldText={getFieldText}
+        selectedLanguage={selectedLanguage}
+      />
+      <Process
+        getFieldText={getFieldText}
+        getExtendedFieldText={getExtendedFieldText}
+        selectedLanguage={selectedLanguage}
+      />
     </form>
   );
 };
