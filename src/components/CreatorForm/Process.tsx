51ad1f57988a995159deefc0e3aa50a753b194e7
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-07-28 17:09:02 -0300

    Update Process w/ ItemRegister

--- src/components/CreatorForm/Process.tsx
+++ src/components/CreatorForm/Process.tsx
@@ -1,10 +1,16 @@
 import { developedWithOptions } from "@/utils/developedWithOptions";
-import React from "react";
 import { SectionProps } from "./CreatorForm";
 import TagInput from "../TagInput/TagInput";
 import TextareaInput from "../TextareaInput/TextareaInput";
+import ItemRegister from "../ItemRegister/ItemRegister";
 
-const Process = ({ getFieldText, selectedLanguage }: SectionProps) => {
+const Process = ({
+  getFieldText,
+  getExtendedFieldText,
+  selectedLanguage,
+}: SectionProps) => {
+  if (getExtendedFieldText === undefined) return;
+
   return (
     <div className="form__section">
       <h2 className="form__section-title">Processo</h2>
@@ -18,6 +24,51 @@ const Process = ({ getFieldText, selectedLanguage }: SectionProps) => {
         label={getFieldText("whatILearned", "value")}
         placeholder={getFieldText("whatILearned", "placeholder")}
         description={getFieldText("whatILearned", "description")}
+      />
+      {/* Useful resources */}
+      <ItemRegister
+        title="Recursos úteis"
+        textarea={{
+          label: getExtendedFieldText("usefulResources", "description", "value"),
+          description: getExtendedFieldText(
+            "usefulResources",
+            "description",
+            "description"
+          ),
+          placeholder: getExtendedFieldText(
+            "usefulResources",
+            "description",
+            "placeholder"
+          ),
+        }}
+        fields={{
+          left: {
+            label: getExtendedFieldText("usefulResources", "solution", "value"),
+            description: getExtendedFieldText(
+              "usefulResources",
+              "solution",
+              "description"
+            ),
+            placeholder: getExtendedFieldText(
+              "usefulResources",
+              "solution",
+              "placeholder"
+            ),
+          },
+          right: {
+            label: getExtendedFieldText("usefulResources", "link", "value"),
+            description: getExtendedFieldText(
+              "usefulResources",
+              "link",
+              "description"
+            ),
+            placeholder: getExtendedFieldText(
+              "usefulResources",
+              "link",
+              "placeholder"
+            ),
+          }
+        }}
       />
     </div>
   );
