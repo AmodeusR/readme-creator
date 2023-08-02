1b0dcf2e66e579d33f5238366dbe7fece9c24202
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-08-01 22:22:12 -0300

    Update Header inputs w/ Redux data handler

--- src/components/CreatorForm/Header.tsx
+++ src/components/CreatorForm/Header.tsx
@@ -1,8 +1,16 @@
+"use client";
+
 import MantineInput from "../MantineInput/MantineInput";
 import ImageDropzone from "../ImageDropzone/ImageDropzone";
 import { SectionProps } from "./CreatorForm";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { setHeaderDescription, setHeaderTitle } from "@/redux/slices/readmeSlice";
 
 const Header = ({ getFieldText, selectedLanguage }: SectionProps) => {
+  const { header } = useAppSelector(state => state.readme);
+  const dispatch = useAppDispatch();
+
+  
   return (
     <div className="form__section">
       <h2 className="form__section-title">Cabeçalho</h2>
@@ -11,11 +19,15 @@ const Header = ({ getFieldText, selectedLanguage }: SectionProps) => {
         label={getFieldText("title", "value")}
         placeholder={getFieldText("title", "placeholder")}
         description={getFieldText("title", "description")}
+        value={header.title}
+        onChange={(e) => dispatch(setHeaderTitle(e.target.value))}
       />
       <MantineInput
         label="Descrição"
         placeholder={getFieldText("description", "placeholder")}
         description={getFieldText("description", "description")}
+        value={header.description}
+        onChange={(e) => dispatch(setHeaderDescription(e.target.value))}
       />
       <ImageDropzone selectedLanguage={selectedLanguage} />
     </div>
