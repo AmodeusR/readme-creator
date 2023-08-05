983a7297bc19680e96d21f981a739c28d1534ea9
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-08-04 22:13:40 -0300

    Add new actions and expand state
    State expansion: overview
    Actions added: setChallenges, setScreenshots & removeScreenshots

--- src/redux/slices/readmeSlice.ts
+++ src/redux/slices/readmeSlice.ts
@@ -1,4 +1,8 @@
-import { Image } from "@/components/ImageDropzone/ImageDropzone";
+import {
+  Image,
+  ImageWithPosition,
+} from "@/components/ImageDropzone/ImageDropzone";
+import { Positions } from "@/components/ImageDropzone/SmallImagreDropzone";
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
@@ -7,16 +11,35 @@ type InitialState = {
     title: string;
     description: string;
     image: Image | null;
-  }
-}
+  };
+  overview: {
+    challenges: string;
+    images: {
+      1: Image | null;
+      2: Image | null;
+      3: Image | null;
+    };
+    links: string;
+  };
+};
 
 const initialState: InitialState = {
   header: {
     title: "",
     description: "",
-    image: null
-  }
-}
+    image: null,
+  },
+  overview: {
+    challenges: "",
+    images: {
+      1: null,
+      2: null,
+      3: null,
+    },
+    links: "",
+  },
+};
+
 const readmeSlice = createSlice({
   name: "readme",
   initialState,
@@ -28,14 +51,36 @@ const readmeSlice = createSlice({
       state.header.description = action.payload;
     },
     // Non-serializable value being used: Image
-    setHeaderImage: (state, action: PayloadAction<Image>) => { 
-      state.header.image = action.payload;
+    setHeaderImage: (state, action: PayloadAction<Image>) => {
+        state.header.image = action.payload;
     },
-    removeHeaderImage: (state) => { 
+    removeHeaderImage: (state) => {
       state.header.image = null;
     },
-  }
+    setChallenges: (state, action: PayloadAction<string>) => {
+      state.overview.challenges = action.payload;
+    },
+    setScreenshots: (
+      state,
+      action: PayloadAction<ImageWithPosition>
+    ) => {
+      if (!action.payload.position) return;
+      const position = action.payload.position;
+      state.overview.images[position] = action.payload.payload;
+    },
+    removeScreenshots: (state, action: PayloadAction<Positions>) => {
+      state.overview.images[action.payload] = null;
+    }
+  },
 });
 
-export const { setHeaderTitle, setHeaderDescription, setHeaderImage, removeHeaderImage } = readmeSlice.actions;
+export const {
+  setHeaderTitle,
+  setHeaderDescription,
+  setHeaderImage,
+  removeHeaderImage,
+  setChallenges,
+  setScreenshots,
+  removeScreenshots
+} = readmeSlice.actions;
 export default readmeSlice.reducer;
