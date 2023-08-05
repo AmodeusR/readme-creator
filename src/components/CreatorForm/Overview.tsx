805d433d7ef9a0f916d458cba2592fbc37c16357
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-08-04 22:11:31 -0300

    Update Overview with Small Image Dropzone

--- src/components/CreatorForm/Overview.tsx
+++ src/components/CreatorForm/Overview.tsx
@@ -1,9 +1,20 @@
 import MantineInput from "../MantineInput/MantineInput";
-import ImageDropzone from "../ImageDropzone/ImageDropzone";
 import { SectionProps } from "./CreatorForm";
 import TextareaInput from "../TextareaInput/TextareaInput";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import {
+  removeScreenshots,
+  setChallenges,
+  setScreenshots,
+} from "@/redux/slices/readmeSlice";
+import SmallImageDropzone from "../ImageDropzone/SmallImagreDropzone";
 
 const Overview = ({ getFieldText, selectedLanguage }: SectionProps) => {
+  const { challenges, images, links } = useAppSelector(
+    (state) => state.readme.overview
+  );
+  const dispatch = useAppDispatch();
+
   return (
     <div className="form__section form__inputs">
       <h2 className="form__section-title">Sessão Geral</h2>
@@ -11,13 +22,33 @@ const Overview = ({ getFieldText, selectedLanguage }: SectionProps) => {
         label={getFieldText("challenges", "value")}
         placeholder={getFieldText("challenges", "placeholder")}
         description={getFieldText("challenges", "description")}
+        value={challenges}
+        onChange={(e) => dispatch(setChallenges(e.target.value))}
       />
       <div>
         <p className="form__screen-captures-title">Capturas de Tela</p>
         <div className="form__screen-captures">
-          <ImageDropzone selectedLanguage={selectedLanguage} size="small" />
-          <ImageDropzone selectedLanguage={selectedLanguage} size="small" />
-          <ImageDropzone selectedLanguage={selectedLanguage} size="small" />
+          <SmallImageDropzone
+            selectedLanguage={selectedLanguage}
+            imageOrigin={images[1]}
+            setImageFunction={setScreenshots}
+            removeImageFunction={removeScreenshots}
+            position={1}
+          />
+          <SmallImageDropzone
+            selectedLanguage={selectedLanguage}
+            imageOrigin={images[2]}
+            setImageFunction={setScreenshots}
+            removeImageFunction={removeScreenshots}
+            position={2}
+          />
+          <SmallImageDropzone
+            selectedLanguage={selectedLanguage}
+            imageOrigin={images[3]}
+            setImageFunction={setScreenshots}
+            removeImageFunction={removeScreenshots}
+            position={3}
+          />
         </div>
       </div>
       <TextareaInput
