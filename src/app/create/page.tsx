56fe4de42d3f4303aa0dd11155da5a0b6abb1745
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-08-08 20:53:31 -0300

    Remove props from Create page

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,24 +1,16 @@
 "use client";
-
-import { useState } from "react";
+// ? For some reason, this component needs to be a client component because of the imports below needing flushSync from React
 import { CreatorForm, CreatorLanguages } from "../../components";
-import { LanguageCode } from "../../lang/form-fields-text";
 import "./creator.scss";
 
 
 const Creator = () => {
-  const [selectedLanguage, setSelectedLanguage] = useState<LanguageCode>("pt-br");
-
-  const handleSelection = (langcode: LanguageCode) => {
-    setSelectedLanguage(langcode);
-  };
-
   return (
     <div className="creator__wrapper">
       <h1 className="creator__title">Criador</h1>
-      <CreatorLanguages selectedLanguage={selectedLanguage} handleSelection={handleSelection} />
+      <CreatorLanguages />
       <main className="creator">
-        <CreatorForm selectedLanguage={selectedLanguage} />
+        <CreatorForm />
         <hr className="creator__separator" />
         <div className="creator__preview">
           <p>Preview</p>
