6f191cdb3d6b06c3332559796f18da95ed4a83f9
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-08-07 21:56:21 -0300

    Update Author w/ AuthorSocialsItems

--- src/components/CreatorForm/Author.tsx
+++ src/components/CreatorForm/Author.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import AuthorSocialsItems from "../AuthorSocialsItems/AuthorSocialsItems";
+import { SectionProps } from "./CreatorForm";
 
-const Author = () => {
+const Author = ({ selectedLanguage, getExtendedFieldText}: SectionProps) => {
+  if (getExtendedFieldText === undefined) return;
+
   return (
     <div className="form__section">
-      <h2 className="form__section-title">Sessão Geral</h2>
+      <h2 className="form__section-title">Autor</h2>
+      <AuthorSocialsItems
+        title="Referências"
+        fields={{
+          left: {
+            label: getExtendedFieldText("references", "platform", "value"),
+            description: getExtendedFieldText("references", "platform", "description"),
+            placeholder: getExtendedFieldText("references", "platform", "placeholder"),
+          },
+          right: {
+            label: getExtendedFieldText("references", "link", "value"),
+            description: getExtendedFieldText("references", "link", "description"),
+            placeholder: getExtendedFieldText("references", "link", "placeholder"),
+          },
+        }}
+      />
     </div>
   );
 };
