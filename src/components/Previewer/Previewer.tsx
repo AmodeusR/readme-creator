28b79715e96660116970fcbf223811739ac64bba
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-08-11 11:35:29 -0300

    Update Previewer w/ Creator Header data

--- src/components/Previewer/Previewer.tsx
+++ src/components/Previewer/Previewer.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+"use client";
+
+import { useAppSelector } from "@/redux/hooks";
+import "./previewer.scss";
 
 const Previewer = () => {
+  const { header, overview, process, author } = useAppSelector(state => state.readme);
   return (
-    <div>Previewer</div>
-  )
-}
+    <div className="creator__previewer">
+      <h2 className="center">{header.title}</h2>
+      <p className="center">{header.description}</p>
+      {header.image &&
+        <img src={header.image.preview} alt="User's readme header image" />
+      }
+    </div>
+  );
+};
 
-export default Previewer
+export default Previewer;
