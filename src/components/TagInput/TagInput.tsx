560c5cf3698824fa096bb04feadb57f1983e8a98
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-08-14 11:21:34 -0300

    Update TagInput.tsx
    TagInput updates:
    
    - It's now creatable (can create values)
    - Receives a dispatcher
    - To support custom prop, a specific type was created
    - Icon now is passed as a prop instead of being default in the component

--- src/components/TagInput/TagInput.tsx
+++ src/components/TagInput/TagInput.tsx
@@ -2,28 +2,29 @@
 
 import { useEffect, useState } from "react";
 import { MultiSelect, MultiSelectProps } from "@mantine/core";
-import { Tag } from "../../assets";
 import { useStyles } from "@/utils/mantineStyles";
 import { useAppDispatch } from "@/redux/hooks";
-import { setDevelopedWith } from "@/redux/slices/readmeSlice";
+import { AnyAction } from "redux";
+
+interface TagInputProps extends MultiSelectProps {
+  dispatcher: (payload: string[]) => AnyAction
+}
 
 // Input component
 export default function TagInput({
-  ...props
-}: MultiSelectProps) {
+  dispatcher, ...props
+}: TagInputProps) {
   const { classes } = useStyles();
   const [chosenOptions, setChosenOptions] = useState<string[]>([]);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(setDevelopedWith(chosenOptions));
+    dispatch(dispatcher(chosenOptions));
   }, [chosenOptions]);
   
   return (
     <MultiSelect
       size="md"
-      searchable
-      icon={<Tag />}
       classNames={{
         icon: classes.icon,
         value: classes.value,
@@ -31,8 +32,14 @@ export default function TagInput({
         root: classes.root,
         label: classes.label,
       }}
-      value={chosenOptions}
       onChange={setChosenOptions}
+      searchable
+      creatable
+      getCreateLabel={(query) => `+ Criar ${query}`}
+      onCreate={(query) => {
+        setChosenOptions([...chosenOptions, query]);
+        return query;
+      }}
       {...props}
     />
   );
