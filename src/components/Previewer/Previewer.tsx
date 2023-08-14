00d8c2549b560b36b935c7adf363f162fc6a06f1
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-08-13 23:42:01 -0300

    Remove header from Previewer & make Process layout

--- src/components/Previewer/Previewer.tsx
+++ src/components/Previewer/Previewer.tsx
@@ -2,16 +2,33 @@
 
 import { useAppSelector } from "@/redux/hooks";
 import "./previewer.scss";
+import HeaderPreview from "./HeaderPreview";
 
 const Previewer = () => {
   const { header, overview, process, author, creatorFormLanguage } = useAppSelector(state => state.readme);
   return (
     <div className="creator__previewer">
-      <h2 className="center">{header[creatorFormLanguage].title}</h2>
-      <p className="center">{header[creatorFormLanguage].description}</p>
-      {header.image &&
-        <img src={header.image.preview} alt="User's readme header image" />
-      }
+      <HeaderPreview source={header} language={creatorFormLanguage} />      
+      <div className="previewer__overview">
+        
+      </div>
+      <div className="previewer__process">
+        <h3>Processo</h3>
+          {process.developedWith.length > 0 && process.developedWith.map(item => (
+        <>
+          <h4>Desenvolvido com...</h4>
+          <ul>
+              <li key={item}>{item}</li>
+          </ul>
+        </>
+          ))}
+          {process["pt-br"].whatILearned && 
+          <>
+            <h4>O que aprendi</h4>
+            <p>{process["pt-br"].whatILearned}</p>
+          </>
+          }
+      </div>
     </div>
   );
 };
